b36e5089758071af9f910616e19155aef81b6994
Yuya Matsumura <matsumurayuuya@yuya-no-MacBook-Pro.local>
2017-03-12 22:47:47 +0900

    0.0.9000 xgb_opt

---
+++ R/xgb_opt.R
@@ -0,0 +1,117 @@
+##' Bayesian optimization for xgboost
+##'
+##' This function estimates parameters for xgboost based on bayesian optimization.
+##' @param train_data A data frame for training of xgboost
+##' @param train_label The column of class to classify in the training data
+##' @param test_data A data frame for training of xgboost
+##' @param test_label The column of class to classify in the test data
+##' @param objectfun Specify the learning task and the corresponding learning objective
+##' \itemize{
+#'     \item \code{reg:linear} linear regression (Default).
+#'     \item \code{reg:logistic} logistic regression.
+#'     \item \code{binary:logistic} logistic regression for binary classification. Output probability.
+#'     \item \code{binary:logitraw} logistic regression for binary classification, output score before logistic transformation.
+#'     \item \code{num_class} set the number of classes. To use only with multiclass objectives.
+#'     \item \code{multi:softmax} set xgboost to do multiclass classification using the softmax objective. Class is represented by a number and should be from 0 to \code{num_class - 1}.
+#'     \item \code{multi:softprob} same as softmax, but prediction outputs a vector of ndata * nclass elements, which can be further reshaped to ndata, nclass matrix. The result contains predicted probabilities of each data point belonging to each class.
+#'     \item \code{rank:pairwise} set xgboost to do ranking task by minimizing the pairwise loss.
+#'   }
+##' @param evalmetric evaluation metrics for validation data. Users can pass a self-defined function to it. Default: metric will be assigned according to objective(rmse for regression, and error for classification, mean average precision for ranking). List is provided in detail section.
+##' @param init_points Number of randomly chosen points to sample the
+#'   target function before Bayesian Optimization fitting the Gaussian Process.
+##' @param n_iter Total number of times the Bayesian Optimization is to repeated.
+##' @param acq Acquisition function type to be used. Can be "ucb", "ei" or "poi".
+#' \itemize{
+#'   \item \code{ucb} GP Upper Confidence Bound
+#'   \item \code{ei} Expected Improvement
+#'   \item \code{poi} Probability of Improvement
+#' }
+##' @param kappa tunable parameter kappa of GP Upper Confidence Bound, to balance exploitation against exploration,
+#'   increasing kappa will make the optimized hyperparameters pursuing exploration.
+##' @param eps  tunable parameter epsilon of Expected Improvement and Probability of Improvement, to balance exploitation against exploration,
+#'   increasing epsilon will make the optimized hyperparameters are more spread out across the whole range.
+##' @param classes set the number of classes. To use only with multiclass objectives.
+##' @return Best parameters for xgboost.
+##'
+##' @import xgboost
+##' @importFrom Matrix sparse.model.matrix
+##' @import rBayesianOptimization
+##' @export
+##'
+xgb_opt <- function(train_data,
+                    train_label,
+                    test_data,
+                    test_label,
+                    objectfun,
+                    evalmetric,
+                    init_points = 20,
+                    n_iter = 1,
+                    acq = "ei",
+                    kappa = 2.576,
+                    eps = 0.0,
+                    classes = NULL
+)
+{
+
+  train_mx <- sparse.model.matrix(train_label ~ ., train_data)
+  test_mx <- sparse.model.matrix(test_label ~ ., test_data)
+
+  if (class(train_label) == "factor"){
+    dtrain <- xgb.DMatrix(train_mx, label = as.integer(train_label) - 1)
+  } else{
+    dtrain <- xgb.DMatrix(train_mx, label = train_label)}
+
+
+  if (class(test_label) == "factor"){
+    dtest <- xgb.DMatrix(test_mx, label = as.integer(test_label) - 1)
+
+  } else{
+    dtest <- xgb.DMatrix(test_mx, label = test_label)}
+
+
+  xgb_holdout <- function(object_fun,
+                          eval_met,
+                          num_classes,
+                          eta_opt,
+                          max_depth_opt,
+                          nrounds_opt,
+                          subsample_opt,
+                          bytree_opt) {
+
+    object_fun <- objectfun
+    eval_met <- evalmetric
+    num_classes <- classes
+
+    model <- xgb.train(params = list(objective = object_fun,
+                                     num_class = num_classes,
+                                     eval_metric = eval_met,
+                                     eta = eta_opt,
+                                     max_depth = max_depth_opt,
+                                     subsample = subsample_opt,
+                                     colsample_bytree = bytree_opt),
+                       data = dtrain,
+                       nrounds = nrounds_opt)
+    t_pred <- predict(model, newdata = dtest)
+    Pred <- sum(diag(table(test_label, t_pred)))/nrow(test_data)
+    list(Score = Pred, Pred = Pred)
+  }
+
+
+  opt_res <- BayesianOptimization(xgb_holdout,
+                                  bounds = list(eta_opt = c(0.1, 1L),
+                                                max_depth_opt = c(4L, 6L),
+                                                nrounds_opt = c(70L, 160L),
+                                                subsample_opt = c(0.1, 1L),
+                                                bytree_opt = c(0.4, 1L)),
+                                  init_points,
+                                  init_grid_dt = NULL,
+                                  n_iter,
+                                  acq,
+                                  kappa,
+                                  eps,
+                                  verbose = TRUE)
+
+  return(opt_res)
+
+}
+
