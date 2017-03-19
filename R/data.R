ce3f915ed81078d0fe4320228e8d6a97ce188c14
Yuya Matsumura <matsumurayuuya@yuya-no-MacBook-Pro.local>
2017-03-19 11:23:49 +0900

    data documentation

---
+++ R/data.R
@@ -0,0 +1,28 @@
+#'  odd-numbered rows of iris data
+#'
+#'  odd-numbered rows of iris data
+#'
+"iris_train"
+
+#'  even-numbered rows of iris data
+#'
+#'  even-numbered rows of iris data
+#'
+"iris_test"
+
+#' produced from kaggle competition
+#'
+#' produced from kaggle competition
+#'
+#' @source \url{https://github.com/ozt-ca/tjo.hatenablog.samples/tree/master/r_samples/public_lib/jp/mnist_reproduced}
+#'
+"train_mnist"
+
+#' produced from kaggle competition
+#'
+#' produced from kaggle competition
+#'
+#' @source \url{https://github.com/ozt-ca/tjo.hatenablog.samples/tree/master/r_samples/public_lib/jp/mnist_reproduced}
+#'
+"test_mnist"
+
