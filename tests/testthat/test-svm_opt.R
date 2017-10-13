10edbea13b6028725806becbce5b5486eed4f92c
松村優哉 <yuya@matsumurayuuyanoMacBook-Pro.local>
2017-10-14 00:08:30 +0900

    test-svm_opt()

--- tests/testthat/test-svm_opt.R
+++ tests/testthat/test-svm_opt.R
@@ -6,13 +6,12 @@ data(iris_train, package = "MlBayesOpt")
 data(iris_test, package = "MlBayesOpt")
 
 set.seed(71)
-res0 <- xgb_opt(train_data = iris_train,
+res0 <- svm_opt(train_data = iris_train,
                 train_label = Species,
                 test_data = iris_test,
                 test_label = Species,
-                objectfun = "multi:softmax",
-                evalmetric = "merror",
-                classes = 3,
+                svm_kernel = "sigmoid",
+                kappa = 10,
                 init_points = 20,
                 n_iter = 1)
 str(res0)
