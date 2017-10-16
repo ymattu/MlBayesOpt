b7e7831159aa9d3cc35d96a231aa22ed2cf53438
Yuya Matsumura <matt.yuya@gmail.com>
2017-10-16 15:40:46 +0900

    update document

--- R/data.R
+++ R/data.R
@@ -1,34 +1,34 @@
-#'  odd-numbered rows of iris data
+#'  Odd-numbered rows of iris data
 #'
-#'  odd-numbered rows of iris data
+#'  Odd-numbered rows of iris data
 #'
 "iris_train"
 
-#'  even-numbered rows of iris data
+#'  Even-numbered rows of iris data
 #'
-#'  even-numbered rows of iris data
+#'  Even-numbered rows of iris data
 #'
 "iris_test"
 
-#' produced from fashion-mnist data
+#' Reproduced from fashion-mnist data
 #'
-#' produced from fashion-mnist data
+#' Reproduced from fashion-mnist data
 #'
 #' @source \url{https://github.com/ymattu/fashion-mnist-csv}
 #'
 "fashion_train"
 
-#' produced from fashion-mnist data
+#' Reproduced from fashion-mnist data
 #'
-#' produced from fashion-mnist data
+#' Reproduced from fashion-mnist data
 #'
 #' @source \url{https://github.com/ymattu/fashion-mnist-csv}
 #'
 "fashion_test"
 
-#' produced from fashion-mnist data
+#' Reproduced from fashion-mnist data
 #'
-#' produced from fashion-mnist data
+#' Reproduced from fashion-mnist data
 #'
 #' @source \url{https://github.com/ymattu/fashion-mnist-csv}
 #'
